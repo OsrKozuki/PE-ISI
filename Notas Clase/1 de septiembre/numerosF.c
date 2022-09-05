cd7d8cccf48576cb4be6592b9640bd82fac75c2b
OsrKozuki <oscar.alor@iteso.mx>
2022-09-05 11:33:29 -0500

    PE ISI

---
+++ Notas Clase/1 de septiembre/numerosF.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+// Funcion para numero mayor
+    int num_mas_grande(x, y, z)
+    {
+        imprimir = printf("Imprimo numero %d", z);
+
+        return z;
+    }
+
+
+// Funcion mas grande
+    int main ()
+    {
+        int mayor = 0;
+        int x = 5;
+        int y = 10;
+        int z = 15;
+        return 0;
+        num_mas_grande(imprimir);
+        mayor = num_mas_grande(x, y, z);
+    }
